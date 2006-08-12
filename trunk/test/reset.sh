4c821cd95fd05be640133e1600269c0685f34231
lucholaf <lucholaf@a68ef73d-011a-0410-a5f0-8abad4636255>
2006-08-12 23:18:23 +0000

    tests refactored

--- trunk/test/reset.sh
+++ trunk/test/reset.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-make clean
 iptables -F INPUT
 iptables -P INPUT ACCEPT
 rmmod ipt_pknock
--- trunk/test/test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-iptables -P INPUT DROP
-
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-iptables -A INPUT -p tcp -m state --state NEW -m pknock --dports 2001,2000 --setip --time 10 --name SSH -j DROP
-iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -m pknock --chkip --name SSH -j ACCEPT
