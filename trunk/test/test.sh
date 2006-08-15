83f1c53a706a359b466806bd1873eb155700d456
lucholaf <lucholaf@a68ef73d-011a-0410-a5f0-8abad4636255>
2006-08-15 02:44:52 +0000

    ICA moved

--- trunk/test/test.sh
+++ trunk/test/test.sh
@@ -6,3 +6,6 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -A INPUT -p tcp -m state --state NEW -m pknock --dports 2000,2001 --setip --time 10 --name SSH -j DROP
 iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -m pknock --chkip --name SSH -j ACCEPT
+
+# what about this?:
+# iptables -A INPUT -p tcp -m state --state NEW -m pknock --knock-ports 2000,2001 --door-port 22 --name SSH -j ACCEPT
