5b625534dae2369a35a60a08e6799fd96d36d5ed
fender <fender@a68ef73d-011a-0410-a5f0-8abad4636255>
2009-07-01 16:17:16 +0000

    Update to the version 1.4.x.

--- trunk/iptables/libipt_pknock.c
+++ trunk/iptables/libipt_pknock.c
@@ -2,7 +2,7 @@
  * Shared library add-on to iptables to add Port Knocking and SPA matching
  * support.
  *
- * (C) 2006-2007 J. Federico Hernandez <fede.hernandez@gmail.com>
+ * (C) 2006-2009 J. Federico Hernandez <fede.hernandez@gmail.com>
  * (C) 2006 Luis Floreani <luis.floreani@gmail.com>
  *
  * $Id$
@@ -14,26 +14,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <iptables.h>
+#include <xtables.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4/ip_tables.h>
 //#include <linux/netfilter_ipv4/ipt_pknock.h>
 #include "../kernel/ipt_pknock.h"
 
-static struct option opts[] = {
-	{ .name = "knockports",	.has_arg = 1,	.flag = 0, .val = 'k' },
-	{ .name = "t",		.has_arg = 1,	.flag = 0, .val = 't' },
-	{ .name = "time",	.has_arg = 1,	.flag = 0, .val = 't' },
-	{ .name = "name",	.has_arg = 1,	.flag = 0, .val = 'n' },
-	{ .name = "opensecret",	.has_arg = 1,	.flag = 0, .val = 'a' },
-	{ .name = "closesecret",.has_arg = 1,	.flag = 0, .val = 'z' },
-	{ .name = "strict",	.has_arg = 0,	.flag = 0, .val = 'x' },
-	{ .name = "checkip",	.has_arg = 0,	.flag = 0, .val = 'c' },
-	{ .name = "chkip",	.has_arg = 0,	.flag = 0, .val = 'c' },
-	{ .name = 0 }
+static const struct option pknock_opts[] = {
+	/* .name, .has_arg, .flag, .val */
+	{ "knockports",	1,	0, 'k' },
+	{ "t",			1,	0, 't' },
+	{ "time",		1,	0, 't' },
+	{ "name",		1,	0, 'n' },
+	{ "opensecret",	1,	0, 'a' },
+	{ "closesecret",1,	0, 'z' },
+	{ "strict",		0,	0, 'x' },
+	{ "checkip",	0,	0, 'c' },
+	{ "chkip",		0,	0, 'c' },
+	{ .name = NULL }
 };
 
-static void help(void)
+/* Function which prints out usage message. */
+static void pknock_help(void)
 {
-	printf("Port Knocking match v%s options:\n"
+	printf("pknock match options:\n"
 		" --knockports port[,port,port,...]	"
 			"Matches destination port(s).\n"
 		" --time seconds\n"
@@ -47,255 +51,247 @@ static void help(void)
 			"Rule name.\n"
 		" --checkip				"
 			"Matches if the source ip is in the list.\n"
-		" --chkip\n", IPTABLES_VERSION);
+		" --chkip\n");
 }
 
-static void init(struct ipt_entry_match *m, unsigned int *nfcache)
+static unsigned int
+parse_ports(const char *portstring, u_int16_t *ports, const char *proto)
 {
-	*nfcache |= NFC_UNKNOWN;
-}
+	char *buffer, *cp, *next;
+	unsigned int i;
 
-/**
- * Parsea ports por comas (ej. de ports: "4000,1000,2000"), los
- * convierte a entero y los devuelve en port_buf.
- *
- * @ports
- * @port_buf
- * @count: count ports
- * @return: 0 success, > 0 otherwise
- */
-static int parse_ports(const char *ports, u_int16_t *port_buf, u_int8_t *count)
-{
-	char *token=NULL, *str=NULL;
-	const char *delim = ",";
-	int i;
-	int ret;
+	buffer = strdup(portstring);
+	if (!buffer) xtables_error(OTHER_PROBLEM, "strdup failed");
 
-	if (ports == NULL) return 1;
-
-	if ((str = strdup(ports)) == NULL) return 2;
-
-	for (i=0, token = strtok(str, delim);
-		token != NULL && i < IPT_PKNOCK_MAX_PORTS;
-		token = strtok(NULL, delim), i++, port_buf++)
+	for (cp=buffer, i=0; cp && i<IPT_PKNOCK_MAX_PORTS; cp=next, i++)
 	{
-		ret = string_to_number(token, 0, 65535,
-					(unsigned int *)port_buf);
-		if (ret == -1) {
-			if (str) free(str);
-			return 3;
-		}
-#if DEBUG
-		printf("port[%d]: %d\n", i, *port_buf);
-#endif
+		next=strchr(cp, ',');
+		if (next) *next++='\0';
+		ports[i] = xtables_parse_port(cp, proto);
 	}
-	*count = i;
+	
+	if (cp) xtables_error(PARAMETER_PROBLEM, "too many ports specified");
 
-	if (str) free(str);
-	return 0;
+	free(buffer);
+	return i;
 }
 
-#define EXIT_ERR_REPORT(error_val) do {					\
-	switch (error_val) {						\
-	case 1:								\
-		fprintf(stderr, "%s port[,port,port,...]\n", argv[0]);	\
-		break;							\
-	case 2:								\
-		fprintf(stderr, "There isn't enough memory - strdup().\n"); \
-		break;							\
-	case 3:								\
-		fprintf(stderr, "Port number invalid.\n");		\
-		break;							\
-	}								\
-	exit(EXIT_FAILURE);						\
-} while (0)								\
-
-static int parse(int c, char **argv, int invert, unsigned int *flags,
-		const struct ipt_entry *entry,
-		unsigned int *nfcache,
-		struct ipt_entry_match **match)
+static char *
+proto_to_name(u_int8_t proto)
 {
-	struct ipt_pknock_info *info;
-	int ret=0;
+	switch (proto) {
+	case IPPROTO_TCP:
+		return "tcp";
+	case IPPROTO_UDP:
+		return "udp";
+	default:
+		return NULL;
+	}
+}
 
-	info =  (struct ipt_pknock_info *) (*match)->data;
+static const char *
+check_proto(u_int16_t pnum, u_int8_t invflags)
+{
+	char *proto;
+
+	if (invflags & XT_INV_PROTO)
+		xtables_error(PARAMETER_PROBLEM, PKNOCK "only works with TCP and UDP.");
+
+	if ((proto = proto_to_name(pnum)) != NULL)
+		return proto;
+	else if (!pnum)
+		xtables_error(PARAMETER_PROBLEM, PKNOCK "needs `-p tcp' or `-p udp'");
+	else
+		xtables_error(PARAMETER_PROBLEM, PKNOCK "only works with TCP and UDP.");
+}
+
+/* Function which parses command options; returns true if it ate an option */
+static int 
+__pknock_parse(int c, char **argv, int invert, unsigned int *flags,
+		struct xt_entry_match **match, u_int16_t pnum,
+		u_int16_t invflags)
+{
+	const char *proto;
+	struct ipt_pknock *info = (struct ipt_pknock *) (*match)->data;
 
 	switch (c) {
 	case 'k': /* --knockports */
 		if (*flags & IPT_PKNOCK_KNOCKPORT)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Cant't use --knockports twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --knockports twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD "Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
+		proto = check_proto(pnum, invflags);
 
-
-		ret = parse_ports(optarg, info->port, &(info->count_ports));
-		if (ret != 0)
-			EXIT_ERR_REPORT(ret);
-#if DEBUG
-		printf("count_ports: %d\n", info->count_ports);
-#endif
-		*flags |= IPT_PKNOCK_KNOCKPORT;
+		info->ports_count = parse_ports(optarg, info->port, proto);
 		info->option |= IPT_PKNOCK_KNOCKPORT;
+		*flags |= IPT_PKNOCK_KNOCKPORT;
+#if DEBUG
+		printf("ports_count: %d\n", info->ports_count);
+#endif
 		break;
 
 	case 't': /* --time */
 		if (*flags & IPT_PKNOCK_TIME)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Cant't use --time twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --time twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
 
 		info->max_time = atoi(optarg);
-
-		*flags |= IPT_PKNOCK_TIME;
 		info->option |= IPT_PKNOCK_TIME;
+		*flags |= IPT_PKNOCK_TIME;
 		break;
 
 	case 'n': /* --name */
 		if (*flags & IPT_PKNOCK_NAME)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't use --name twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --name twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
 
 		memset(info->rule_name, 0, IPT_PKNOCK_MAX_BUF_LEN + 1);
 		strncpy(info->rule_name, optarg, IPT_PKNOCK_MAX_BUF_LEN);
+
 		info->rule_name_len = strlen(info->rule_name);
+		info->option |= IPT_PKNOCK_NAME;
+		*flags |= IPT_PKNOCK_NAME;
 #if DEBUG
 		printf("info->rule_name: %s\n", info->rule_name);
 #endif
-		*flags |= IPT_PKNOCK_NAME;
-		info->option |= IPT_PKNOCK_NAME;
 		break;
 
 	case 'a': /* --opensecret */
 		if (*flags & IPT_PKNOCK_OPENSECRET)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't use --opensecret twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --opensecret twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD "Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
 
 		memset(info->open_secret, 0, IPT_PKNOCK_MAX_PASSWD_LEN + 1);
 		strncpy(info->open_secret, optarg, IPT_PKNOCK_MAX_PASSWD_LEN);
-		info->open_secret_len = strlen(info->open_secret);
 
-		*flags |= IPT_PKNOCK_OPENSECRET;
+		info->open_secret_len = strlen(info->open_secret);
 		info->option |= IPT_PKNOCK_OPENSECRET;
+		*flags |= IPT_PKNOCK_OPENSECRET;
 		break;
 
 	case 'z': /* --closesecret */
 		if (*flags & IPT_PKNOCK_CLOSESECRET)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't use --closesecret twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --closesecret twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD "Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
 
 		memset(info->close_secret, 0, IPT_PKNOCK_MAX_PASSWD_LEN + 1);
 		strncpy(info->close_secret, optarg, IPT_PKNOCK_MAX_PASSWD_LEN);
-		info->close_secret_len = strlen(info->close_secret);
 
-		*flags |= IPT_PKNOCK_CLOSESECRET;
+		info->close_secret_len = strlen(info->close_secret);
 		info->option |= IPT_PKNOCK_CLOSESECRET;
+		*flags |= IPT_PKNOCK_CLOSESECRET;
 		break;
 
 	case 'c': /* --checkip */
 		if (*flags & IPT_PKNOCK_CHECKIP)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't use --checkip twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --checkip twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD "Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
 
-		*flags |= IPT_PKNOCK_CHECKIP;
 		info->option |= IPT_PKNOCK_CHECKIP;
+		*flags |= IPT_PKNOCK_CHECKIP;
 		break;
 
 	case 'x': /* --strict */
 		if (*flags & IPT_PKNOCK_STRICT)
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't use --strict twice.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot use --strict twice.\n");
 
-		if(invert)
-			exit_error(PARAMETER_PROBLEM, MOD "Can't specify !.\n");
+		xtables_check_inverse(argv[optind-1], &invert, &optind, 0);
 
-		*flags |= IPT_PKNOCK_STRICT;
 		info->option |= IPT_PKNOCK_STRICT;
+		*flags |= IPT_PKNOCK_STRICT;
 		break;
 
 	default:
 		return 0;
 	}
+
+	if (invert)
+		xtables_error(PARAMETER_PROBLEM, PKNOCK "does not support invert.");
+
 	return 1;
 }
 
-static void final_check(unsigned int flags)
+static int pknock_parse(int c, char **argv, int invert, unsigned int *flags,
+                		const void *e, struct xt_entry_match **match)
+{
+	const struct ipt_entry *entry = e;
+	return __pknock_parse(c, argv, invert, flags, match, 
+			entry->ip.proto, entry->ip.invflags);
+}
+
+/* Final check. */
+static void pknock_check(unsigned int flags)
 {
 	if (!flags)
-		exit_error(PARAMETER_PROBLEM, MOD
-			"You must specify an option.\n");
+		xtables_error(PARAMETER_PROBLEM, PKNOCK "expection an option.\n");
 
 	if (!(flags & IPT_PKNOCK_NAME))
-		exit_error(PARAMETER_PROBLEM, MOD
-			"You must specify --name option.\n");
+		xtables_error(PARAMETER_PROBLEM, PKNOCK
+			"--name option is required.\n");
 
 	if (flags & IPT_PKNOCK_KNOCKPORT) {
 		if (flags & IPT_PKNOCK_CHECKIP) {
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't specify --knockports with --checkip.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot specify --knockports with --checkip.\n");
 		}
 		if ((flags & IPT_PKNOCK_OPENSECRET)
 			&& !(flags & IPT_PKNOCK_CLOSESECRET))
 		{
-			exit_error(PARAMETER_PROBLEM, MOD
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
 				"--opensecret must go with --closesecret.\n");
 		}
 		if ((flags & IPT_PKNOCK_CLOSESECRET)
 			&& !(flags & IPT_PKNOCK_OPENSECRET))
 		{
-			exit_error(PARAMETER_PROBLEM, MOD
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
 				"--closesecret must go with --opensecret.\n");
 		}
 	}
 
 	if (flags & IPT_PKNOCK_CHECKIP) {
 		if (flags & IPT_PKNOCK_KNOCKPORT) {
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't specify --checkip with --knockports.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot specify --checkip with --knockports.\n");
 		}
 		if ((flags & IPT_PKNOCK_OPENSECRET)
 			|| (flags & IPT_PKNOCK_CLOSESECRET))
 		{
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't specify --opensecret and"
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot specify --opensecret and"
 				" --closesecret with --checkip.\n");
 		}
 		if (flags & IPT_PKNOCK_TIME) {
-			exit_error(PARAMETER_PROBLEM, MOD
-				"Can't specify --time with --checkip.\n");
+			xtables_error(PARAMETER_PROBLEM, PKNOCK
+				"cannot specify --time with --checkip.\n");
 		}
 	}
 }
 
-static void print(const struct ipt_ip *ip, const struct ipt_entry_match *match,
-		 int numeric)
+/* Prints out the matchinfo. */
+static void pknock_print(const void *ip, 
+						const struct xt_entry_match *match, int numeric)
 {
-	const struct ipt_pknock_info *info;
+	const struct ipt_pknock *info;
 	int i;
 
-	info = (const struct ipt_pknock_info *)match->data;
+	info = (const struct ipt_pknock *)match->data;
 
 	printf("pknock ");
 	if (info->option & IPT_PKNOCK_KNOCKPORT) {
 		printf("knockports ");
-		for (i=0; i<info->count_ports; i++)
+		for (i=0; i<info->ports_count; i++)
 			printf("%s%d", i ? "," : "", info->port[i]);
 		printf(" ");
 	}
@@ -309,16 +305,15 @@ static void print(const struct ipt_ip *ip, const struct ipt_entry_match *match,
 		printf("closesecret ");
 }
 
-static void save(const struct ipt_ip *ip, const struct ipt_entry_match *match)
+/* Saves the union ipt_matchinfo in parsable form to stdout. */
+static void pknock_save(const void *ip, const struct xt_entry_match *match)
 {
-	const struct ipt_pknock_info *info;
 	int i;
-
-	info = (const struct ipt_pknock_info *)match->data;
+	const struct ipt_pknock *info = (const struct ipt_pknock *)match->data;
 
 	if (info->option & IPT_PKNOCK_KNOCKPORT) {
 		printf("--knockports ");
-		for (i=0; i<info->count_ports; i++)
+		for (i=0; i<info->ports_count; i++)
 			printf("%s%d", i ? "," : "", info->port[i]);
 		printf(" ");
 	}
@@ -336,21 +331,21 @@ static void save(const struct ipt_ip *ip, const struct ipt_entry_match *match)
 		printf("--checkip ");
 }
 
-static struct iptables_match pknock = {
+static struct xtables_match pknock_match = {
 	.name		= "pknock",
-	.version	= IPTABLES_VERSION,
-	.size		= IPT_ALIGN(sizeof (struct ipt_pknock_info)),
-	.userspacesize	= IPT_ALIGN(sizeof (struct ipt_pknock_info)),
-	.help		= &help,
-	.init		= &init,
-	.parse		= &parse,
-	.final_check	= &final_check,
-	.print		= &print,
-	.save		= &save,
-	.extra_opts	= opts
+	.version	= XTABLES_VERSION,
+	.family		= NFPROTO_IPV4,
+	.size		= XT_ALIGN(sizeof (struct ipt_pknock)),
+	.userspacesize	= XT_ALIGN(sizeof (struct ipt_pknock)),
+	.help		= pknock_help,
+	.parse		= pknock_parse,
+	.final_check	= pknock_check,
+	.print		= pknock_print,
+	.save		= pknock_save,
+	.extra_opts	= pknock_opts
 };
 
 void _init(void) 
 {
-	register_match(&pknock);
+	xtables_register_match(&pknock_match);
 }
